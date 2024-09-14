a85606c00a0c2b47fe88fccb1b27ab330ce0f318
Raaj-Shinde <raaajshinde@gmail.com>
2024-09-14 14:18:16 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,26 @@
+//Note : Always Try To compare same datatype . different datatype may be give different output
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+/*
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//these are very complex comparison 
+*/
+// === 
+
+console.log("2" === 2);
