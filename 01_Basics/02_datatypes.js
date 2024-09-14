3ee42f1e046b87ae3d18e09e34302f82f33e709a
Raaj-Shinde <raaajshinde@gmail.com>
2024-09-14 07:14:52 +0000

    datatype

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version 
+
+//alert(3 + 3) //we are using nodejs, not browser
+console.log(3
+    +3) //Code readability should be high 
+
+console.log("RaaJ")
+let state;
+
+// number => 2 to power 53
+// bigInt 
+// String => ""
+// Boolean =>  true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
